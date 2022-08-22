6a83944ed845323adb18453c469c6854d591e735
apps4toddler <business+toddler@satotakuya.jp>
2022-08-22 23:41:59 +0900

    toggle switch

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,67 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  MyAppState createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> with TickerProviderStateMixin {
+  bool _isLightMode = false;
+  bool _isEnglish = false;
   @override
   Widget build(BuildContext context) {
-    return const Text(
-      "レイアウト作成開始中",
-      textDirection: TextDirection.ltr,
-      style: TextStyle(
-        fontSize: 32,
-        color: Colors.black87,
+    return MaterialApp(
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      theme: ThemeData(
+        brightness: Brightness.light,
+      ),
+      themeMode: _isLightMode ? ThemeMode.light : ThemeMode.dark,
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                alignment: Alignment.topRight,
+                width: double.infinity,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Switch(
+                      value: _isLightMode,
+                      onChanged: (bool value) {
+                        setState(() {
+                          _isLightMode = value;
+                        });
+                      },
+                      activeThumbImage:
+                          const AssetImage("assets/images/icon-light.png"),
+                      inactiveThumbImage:
+                          const AssetImage("assets/images/icon-dark.png"),
+                    ),
+                    Switch(
+                      value: _isEnglish,
+                      onChanged: (bool value) {
+                        setState(() {
+                          _isEnglish = value;
+                        });
+                      },
+                      activeThumbImage:
+                          const AssetImage("assets/images/icon-en.png"),
+                      inactiveThumbImage:
+                          const AssetImage("assets/images/icon-jp.png"),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
